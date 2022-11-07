0e527c8ef50947720d1fa33e5311f7d01f488f70
surafel getahun <surafelgetahun01@gmail.com>
2022-11-07 23:59:21 +0300

    main structure

--- clock.py
+++ clock.py
@@ -18,3 +18,60 @@ hour_hand.hideturtle()
 hour =datetime.datetime.now().hour
 minute =datetime.datetime.now().minute
 second=datetime.datetime.now()
+#  structure of the whole clock
+def structure(): 
+    writer.lt(90)
+    hyphen_number=60
+    while hyphen_number >= 1:
+        if hyphen_number%5 == 0:
+            writer.color("brown")
+            writer.penup()
+            writer.fd(200)
+            writer.pendown()
+            writer.pensize(7)
+            writer.fd(20)
+            writer.penup()
+            writer.bk(220)
+            writer.penup()
+        else:
+            writer.color("white")
+            writer.penup()
+            writer.fd(200)
+            writer.pendown()
+            writer.pensize(3)
+            writer.fd(10)
+            writer.penup()
+            writer.bk(210)
+            writer.penup()
+        writer.right(6)
+        hyphen_number = hyphen_number - 1 
+    clock_number = 1
+    while clock_number <= 12:
+        writer.right(30)
+        writer.penup()
+        writer.fd(150)
+        writer.pendown()
+        writer.color("white")
+        writer.write(str(clock_number),align='center',font=('Times New Roman',16,'normal'))
+        writer.penup()
+        writer.bk(150)
+        writer.pendown()
+        clock_number= clock_number + 1
+        writer.hideturtle()
+    hour_hand.pensize(5)
+    hour_hand.color("white")
+    hour_hand.rt(90 + (hour + minute/60 + second/3600)*30)
+    hour_hand.pensize(3)
+    hour_hand.fd(65)
+
+    minute_hand.color("white")
+    minute_hand.pensize(1.8)
+    minute_hand.lt(90)
+    minute_hand.rt((minute + second/60)*6)
+    minute_hand.fd(100)
+
+    second_hand.hideturtle()
+    second_hand.color("white")
+    second_hand.pensize(1.4)
+    second_hand.lt(90)
+    second_hand.rt(second*6)
